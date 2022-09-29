60bb0d062cd89e4d6f975b9683ad590d4c143db5
Luc Perkins <lucperkins@gmail.com>
2022-09-29 10:35:59 -0500

    Add editorconfig check

--- nix/ci.nix
+++ nix/ci.nix
@@ -5,6 +5,9 @@ let
 
   # Use cargo from Rust toolchain
   cargo = "${pkgs.rustToolchain}/bin/cargo";
+
+  # Helper function for running executables
+  run = pkg: "${pkgs.${pkg}}/bin/${pkg}";
 in
 [
   (writeScriptBin "ci-check-rust-formatting" ''
@@ -19,6 +22,10 @@ in
 
   (writeScriptBin "ci-cargo-test" ''
     ${cargo} test --no-fail-fast
+  '')
+
+  (writeScriptBin "ci-check-editorconfig" ''
+    ${run "eclint"} -exclude "Cargo.lock"
   '')
 
   # A helper script for running the CI suite locally
